c7f859919b42a0f17c83a0df38a9690f76748ebd
Ravuthz <ravuthz@gmail.com>
2023-06-06 15:29:53 +0000

    Update script

--- blog/script.js
+++ blog/script.js
@@ -1,6 +1,23 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   document.querySelectorAll("pre").forEach((el) => {
+//     const html = el.innerHTML.trimEnd();
+//     el.innerHTML = hljs.highlightAuto(html).value;
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", (event) => {
-  document.querySelectorAll("pre").forEach((el) => {
-    const html = el.innerHTML.trimEnd();
-    el.innerHTML = hljs.highlightAuto(html).value;
-  });
+
+  const $markdown = document.getElementById("markdown");
+  if ($markdown) {
+    document.getElementById('content').innerHTML = marked.parse($markdown.innerHTML);
+  }
+  
+  const $codes = document.querySelectorAll("pre code");
+  if ($codes) {
+    $codes.forEach((element) => {
+      const html = element.innerHTML.trimEnd();
+      element.innerHTML = hljs.highlightAuto(html).value;
+    });
+  }
+  
 });
